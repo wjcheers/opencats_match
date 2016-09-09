b4d597d12bee3c3e1275f56a50bdba85ebfc7c88
wjcheers <wjc@csie.io>
2016-09-09 16:45:06 +0800

    Fix url / web_site filter works properly

--- lib/DataGrid.php
+++ lib/DataGrid.php
@@ -1139,10 +1139,18 @@ class DataGrid
                 $argument = str_replace(' AND ', '/', $argument);
                 // ~Add
                 
-                /* The / character works as an OR clause for filters. */
-                $argument = str_replace(' or ', '/', $argument);
-                $argument = str_replace(' OR ', '/', $argument);
-                $arguments = explode('/', $argument);
+                /* The / character works as an OR clause for filters, exclude url and web_site */
+                if((strpos($this->_classColumns[$columnName]['filter'], 'web_site') !== false) ||
+                   (strpos($this->_classColumns[$columnName]['filter'], 'url') !== false))
+                {
+                    $arguments = array($argument);
+                }
+                else
+                {
+                    $argument = str_replace(' or ', '/', $argument);
+                    $argument = str_replace(' OR ', '/', $argument);
+                    $arguments = explode('/', $argument);
+                }
 
                 $whereSQL_or = array();
                 $havingSQL_or = array();
